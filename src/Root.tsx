0407b083ef450ee8d517110d8c4a803e125b1f67
Diana Darienko <diana.darienko21@gmail.com>
2025-01-22 13:42:02 +0200

    commit

---
+++ src/Root.tsx
@@ -0,0 +1,27 @@
+import {
+  HashRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from 'react-router-dom';
+import { App } from './App';
+import { HomePage } from './pages/HomePage';
+import { NotFoundPage } from './pages/NotFoundPage';
+import { TabsPage } from './pages/TabsPage';
+
+export const Root = () => (
+  <Router>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route index element={<HomePage />} />
+        <Route path="home" element={<Navigate to="/" />} />
+
+        <Route path="tabs">
+          <Route path=":tabId?" element={<TabsPage />} />
+        </Route>
+
+        <Route path="*" element={<NotFoundPage />} />
+      </Route>
+    </Routes>
+  </Router>
+);
---
+++ src/pages/TabsPage.tsx
@@ -0,0 +1,10 @@
+import { TabsList } from '../components/TabsList';
+
+export const TabsPage = () => {
+  return (
+    <>
+      <h1 className="title">Tabs page</h1>
+      <TabsList />
+    </>
+  );
+};
