45428cb7229c239317defcc1f6f181c8db1087e3
Gustavo Finch <gustavo.finch@gmail.com>
2025-02-04 11:47:08 -0300

    ReactTabsWithRouter

--- src/App.tsx
+++ src/App.tsx
@@ -12,9 +12,9 @@ import './App.scss';
 import classNames from 'classnames';
 
 const tabs = [
-  { id: 'tab-1', title: 'Tab 1', content: 'Escriba Algo' },
-  { id: 'tab-2', title: 'Tab 2', content: 'Escriba Algo' },
-  { id: 'tab-3', title: 'Tab 3', content: 'Escriba Algo' },
+  { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
+  { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
+  { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
 ];
 
 export const Navigation = () => {
