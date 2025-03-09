7f9087a7765ddcb4ec5c68577acb1b0bab32d14c
Mykola Melnichuk <nikolfutdeveloper@gmail.com>
2025-03-09 12:17:55 +0100

    made

--- src/pages/TabsPage.tsx
+++ src/pages/TabsPage.tsx
@@ -17,7 +17,11 @@ const TabsPage = () => {
       <div className="tabs is-boxed">
         <ul>
           {tabs.map(tab => (
-            <li data-cy="Tab" key={tab.id} className={tab.id === tabId ? 'is-active' : ''}>
+            <li
+              data-cy="Tab"
+              key={tab.id}
+              className={tab.id === tabId ? 'is-active' : ''}
+            >
               <Link to={`/tabs/${tab.id}`}>{tab.title}</Link>
             </li>
           ))}
