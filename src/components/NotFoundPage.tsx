2de0f50c137aaa11c94ebd95b33cd0a1479faeee
Oleksandr Kovalchuk <kov280396@gmail.com>
2025-03-22 20:37:27 +0200

    implemented React Tabs with Router

---
+++ src/components/NotFoundPage.tsx
@@ -0,0 +1,9 @@
+const NotFoundPage = () => (
+  <div className="section">
+    <div className="container">
+      <h1 className="title">Page not found</h1>
+    </div>
+  </div>
+);
+
+export default NotFoundPage;
