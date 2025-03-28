2abf81a5a99d3e8419622ffbd75a9772e03d47e9
VadimKudrenko <48049362+VadimKudrenko@users.noreply.github.com>
2025-03-28 14:00:33 +0200

    fix solution

--- src/App.tsx
+++ src/App.tsx
@@ -41,7 +41,10 @@ export const App = () => (
           <Route path="/">
             <Route index element={<h1 className="title">Home page</h1>} />
             <Route path="home" element={<Navigate to=".." />} />
-            <Route path="tabs/:tabId?" element={<TabsPage />} />
+            <Route path="tabs">
+              <Route index element={<TabsPage />} />
+              <Route path=":tabId?" element={<TabsPage />} />
+            </Route>
             <Route
               path="*"
               element={<h1 className="title">Page not found</h1>}
