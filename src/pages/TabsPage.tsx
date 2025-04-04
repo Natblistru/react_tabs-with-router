347798bdba2012b38240a17e176b558653edd93f
PiekharievaK <pechareva@gmail.com>
2025-04-04 16:57:41 +0300

    add solution

--- src/pages/TabsPage.tsx
+++ src/pages/TabsPage.tsx
@@ -1,11 +1,6 @@
-import { NavLink, useParams } from 'react-router-dom';
-import cn from 'classnames';
-
-const tabs = [
-  { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
-  { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
-  { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
-];
+import { useParams } from 'react-router-dom';
+import { tabs } from '../api/tabs';
+import { Tab } from '../components/Tab';
 
 export const TabsPage = () => {
   const { tabId } = useParams();
@@ -18,19 +13,9 @@ export const TabsPage = () => {
 
       <div className="tabs is-boxed">
         <ul>
-          {tabs.map(tab => {
-            return (
-              <li
-                key={tab.id}
-                data-cy="Tab"
-                className={cn({ 'is-active': tab.id === tabId })}
-              >
-                <NavLink to={`/tabs/${tab.id}`} end>
-                  {tab.title}
-                </NavLink>
-              </li>
-            );
-          })}
+          {tabs.map(tab => (
+            <Tab tab={tab} key={tab.id} tabId={tabId} />
+          ))}
         </ul>
       </div>
 
