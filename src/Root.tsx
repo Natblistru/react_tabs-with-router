07270cc15a898bd6ad792ef7f4dd0faf85cb7f11
Daria Radtkina <dasharadtkina@gmail.com>
2025-04-04 17:03:36 +0300

    Solution

---
+++ src/Root.tsx
@@ -0,0 +1,24 @@
+import { HashRouter, Navigate, Route, Routes } from 'react-router-dom';
+import { App } from './App';
+import { TabsPage } from './components/TabsPage';
+import { HomePage } from './components/HomePage';
+
+export const Root = () => (
+  <HashRouter>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route index element={<HomePage />}></Route>
+        <Route path="/home" element={<Navigate to="/" replace />} />
+        <Route path="tabs">
+          <Route index element={<TabsPage />} />
+          <Route path=":tabId" element={<TabsPage />} />
+        </Route>
+        <Route
+          path="*"
+          element={<h1 className="title">Page not found</h1>}
+        ></Route>
+      </Route>
+    </Routes>
+    <App />
+  </HashRouter>
+);
---
+++ src/components/TabsPage.tsx
@@ -0,0 +1,42 @@
+import cn from 'classnames';
+import { Tab } from '../types/Tab';
+import { Link, useParams } from 'react-router-dom';
+
+const tabs: Tab[] = [
+  { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
+  { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
+  { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
+];
+
+export const TabsPage = () => {
+  const { tabId } = useParams();
+  const currentTabId = tabId || 0;
+  const currentTab = tabs.find((tab: Tab) => tab.id === tabId);
+
+  return (
+    <div data-cy="TabsComponent">
+      <h1 className="title">Tabs page</h1>
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => (
+            <li
+              className={cn({
+                'is-active': currentTabId === tab.id,
+              })}
+              data-cy="Tab"
+              key={tab.id}
+            >
+              <Link to={`/tabs/${tab.id}`} data-cy="TabLink">
+                {tab.title}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {currentTab?.content ? currentTab?.content : 'Please select a tab'}
+      </div>
+    </div>
+  );
+};
