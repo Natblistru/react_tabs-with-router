31cc4d0d3301c5822df9e398078607c65267dfb4
beliyandr <beliyandr@gmail.com>
2025-04-04 23:26:06 +0300

    432

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import { App } from './App';
 import { HomePage } from './pages/HomePage';
 import { TabsPage } from './pages/TabsPage';
@@ -10,7 +10,8 @@ export const Root = () => (
 
     <Routes>
       <Route path="/" element={<App />}>
-        <Route index element={<HomePage />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/home" element={<Navigate to="/" />} />
         <Route path="tabs">
           <Route index element={<TabsPage />} />
           <Route path=":tabId" element={<TabsPage />} />
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 export const HomePage = () => {
   return <h1>HomePage</h1>;
 };
