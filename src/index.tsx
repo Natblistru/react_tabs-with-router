fa31c543f17674ce4f01757ebc609bc00c95833e
Mariia Kapustkina <kapustkina08@mail.com>
2025-04-09 16:03:31 +0300

    solution

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,32 @@
 import { createRoot } from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
+import {
+  Navigate,
+  Route,
+  HashRouter as Router,
+  Routes,
+} from 'react-router-dom';
 import { App } from './App';
+import React from 'react';
+import { HomePage } from './components/HomePage';
+import { TabsPage } from './components/TabsPage';
+import { tabs } from './utils/tabsData';
 
-createRoot(document.getElementById('root') as HTMLElement).render(
-  <HashRouter>
-    <App />
-  </HashRouter>,
+const Root = () => (
+  <Router>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route path="/home" element={<Navigate to="/" />} />
+        <Route index element={<HomePage />} />
+        <Route path="tabs">
+          <Route index element={<TabsPage tabs={tabs} />} />
+          <Route path=":tabId" element={<TabsPage tabs={tabs} />} />
+        </Route>
+
+        <Route path="*" element={<h1 className="title">Page not found</h1>} />
+      </Route>
+    </Routes>
+  </Router>
 );
+
+createRoot(document.getElementById('root') as HTMLElement).render(<Root />);
+document.body.classList.add('has-navbar-fixed-top');
