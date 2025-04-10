8e68fa6233cba8ba98dcf532e5aea21ff53414b5
Max Shapes <makc.bilous04@gmail.com>
2025-04-10 21:16:55 +0300

    added routing

---
+++ src/components/TabsPage/TabsPage.tsx
@@ -0,0 +1,44 @@
+import classNames from 'classnames';
+import { Link, useParams } from 'react-router-dom';
+
+const tabs = [
+  { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
+  { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
+  { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
+];
+
+export const TabsPage = () => {
+  const { tabId } = useParams();
+  const selectedTab = tabs.find(tab => tab.id === tabId);
+  const selectedTabId = selectedTab ? selectedTab.id : 'tab-0';
+
+  return (
+    <>
+      <h1 className="title">Tabs page</h1>
+
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => {
+            return (
+              <li
+                key={tab.id}
+                className={classNames({
+                  'is-active': tab.id === selectedTabId,
+                })}
+                data-cy="Tab"
+              >
+                <Link to={`/tabs/${tab.id}`} data-cy="TabLink">
+                  {tab.title}
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {selectedTab ? selectedTab.content : 'Please select a tab'}
+      </div>
+    </>
+  );
+};
