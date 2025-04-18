22f61c9bac4b18416b7401b1c80d3dee9083c93e
Arthur Ogunfuye <arthurity@yahoo.co.uk>
2025-04-18 17:54:25 +0100

    Tabs with router solution

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,30 @@
 import { createRoot } from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
+import {
+  HashRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from 'react-router-dom';
 import { App } from './App';
+import { HomePage } from './pages/Home/HomePage';
+import { NotFoundPage } from './pages/NotFound/NotFoundPage';
+import { TabsPage } from './pages/Tabs/TabsPage';
 
 createRoot(document.getElementById('root') as HTMLElement).render(
-  <HashRouter>
-    <App />
-  </HashRouter>,
+  <Router>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route index element={<HomePage />} />
+
+        <Route path="home" element={<Navigate to="/" replace />} />
+
+        <Route path="tabs">
+          <Route index element={<TabsPage />} />
+          <Route path=":tabId" element={<TabsPage />} />
+        </Route>
+
+        <Route path="*" element={<NotFoundPage />} />
+      </Route>
+    </Routes>
+  </Router>,
 );
