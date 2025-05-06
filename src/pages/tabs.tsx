8b4f96e21f731c961448eeb90320f72cd3772083
LuisCampelo2 <luisfilipecab@gmail.com>
2025-05-06 17:20:14 -0300

    segundo commit

--- src/pages/tabs.tsx
+++ src/pages/tabs.tsx
@@ -7,20 +7,28 @@ export const Tabs = () => {
     {
       id: 'tab-1',
       title: 'Tab 1',
-      content:
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam laudantium eius autem ratione blanditiis sed, consequuntur in molestias fugit natus corrupti iste assumenda soluta fugiat excepturi nobis eaque culpa dicta. Illum excepturi animi tenetur nostrum odit quibusdam reiciendis aut repellendus?',
+      content: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat, minima soluta quasi
+      accusantium mollitia hic facilis explicabo eveniet commodi. Et laudantium, mollitia fuga sed est
+      autem cum animi consequatur, minus, odit aut porro blanditiis quia commodi unde consequuntur enim
+      expedita. Aliquid natus corrupti ad similique quidem velit, maxime non voluptatem?`,
     },
     {
       id: 'tab-2',
       title: 'Tab 2',
-      content:
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat, minima soluta quasi accusantium mollitia hic facilis explicabo eveniet commodi. Et laudantium, mollitia fuga sed est autem cum animi consequatur, minus, odit aut porro blanditiis quia commodi unde consequuntur enim expedita. Aliquid natus corrupti ad similique quidem velit, maxime non voluptatem?',
+      content: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat, minima soluta quasi
+        accusantium mollitia hic facilis explicabo eveniet commodi. Et laudantium, mollitia fuga
+        sed est autem cum animi consequatur, minus, odit aut porro blanditiis quia commodi unde
+        consequuntur enim expedita. Aliquid natus corrupti ad similique quidem velit,
+        maxime non voluptatem?`,
     },
     {
       id: 'tab-3',
       title: 'Tab 3',
-      content:
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia velit qui molestias ipsa dolorum fugiat natus. Illum ex enim neque quidem corrupti cumque error quia, accusantium voluptates! Sit rerum id nam porro. Ullam asperiores atque reprehenderit. Explicabo harum excepturi dolores soluta quidem fugiat necessitatibus voluptates, nam fugit repellat sit! Nisi assumenda, deserunt repudiandae nesciunt vero rerum libero iste amet eaque.',
+      content: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia velit qui molestias ipsa
+        dolorum fugiat natus. Illum ex enim neque quidem corrupti cumque error quia, accusantium
+        voluptates! Sit rerum id nam porro. Ullam asperiores atque reprehenderit. Explicabo harum
+        excepturi dolores soluta quidem fugiat necessitatibus voluptates, nam fugit repellat sit!
+        Nisi assumenda, deserunt repudiandae nesciunt vero rerum libero iste amet eaque.`,
     },
   ];
 
