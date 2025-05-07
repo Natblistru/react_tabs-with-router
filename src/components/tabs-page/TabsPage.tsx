8952289fd4cc7c7c8de2b497d3a2238cdca4fb40
sirserg <sirserg05@gmail.com>
2025-05-07 13:14:18 +0300

    feat: add task solution

---
+++ src/components/tabs-page/TabsPage.tsx
@@ -0,0 +1,43 @@
+import cn from 'classnames';
+import { Link, useParams } from 'react-router-dom';
+
+const tabs = [
+  { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
+  { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
+  { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
+];
+
+export const TabsPage = () => {
+  const { tabId } = useParams();
+
+  const correctActiveTabId = tabs.find(tab => tab.id === tabId) ? tabId : 0;
+
+  const activeTab = tabs.find(tab => tab.id === correctActiveTabId);
+
+  return (
+    <>
+      <h1 className="title">Tabs page</h1>
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => (
+            <li
+              className={cn({
+                'is-active': tab.id === correctActiveTabId,
+              })}
+              data-cy="Tab"
+              key={tab.id}
+            >
+              <Link to={`/tabs/${tab.id}`} data-cy="TabLink">
+                {tab.title}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {activeTab ? activeTab.content : 'Please select a tab'}
+      </div>
+    </>
+  );
+};
