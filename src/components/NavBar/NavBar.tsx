8ec82787f080f4f1f6cabf244b8613ed088af276
BoGoBooM <myPikcha@gmail.com>
2025-05-13 17:54:23 +0300

    task solution

---
+++ src/components/NavBar/NavBar.tsx
@@ -0,0 +1,34 @@
+import { Link, useLocation } from 'react-router-dom';
+import classNames from 'classnames';
+
+export const NavBar = () => {
+  const { pathname } = useLocation();
+
+  return (
+    <nav
+      className="navbar is-light is-fixed-top is-mobile has-shadow"
+      data-cy="Nav"
+    >
+      <div className="container">
+        <div className="navbar-brand">
+          <Link
+            to="/"
+            className={classNames('navbar-item', {
+              'is-active': pathname === '/',
+            })}
+          >
+            Home
+          </Link>
+          <Link
+            to="/tabs"
+            className={classNames('navbar-item', {
+              'is-active': pathname.startsWith('/tabs'),
+            })}
+          >
+            Tabs
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
+};
