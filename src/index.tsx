13b1cba6c5fa3da3258ac99b7a8c4e99e56981f1
Alex Marcovych <alexmarcovych@gmail.com>
2025-05-16 16:57:28 +0300

    initial commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,22 @@
 import { createRoot } from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
+import { HashRouter, Navigate, Route, Routes } from 'react-router-dom';
 import { App } from './App';
+import { TabsPage } from './components/TabsPage';
 
-createRoot(document.getElementById('root') as HTMLElement).render(
+const Root = () => (
   <HashRouter>
-    <App />
-  </HashRouter>,
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route index element={<h1 className="title">Home page</h1>} />
+        <Route path="tabs">
+          <Route index element={<TabsPage />} />
+          <Route path=":tabId?" element={<TabsPage />} />
+        </Route>
+        <Route path="/home" element={<Navigate to="/" replace />} />
+        <Route path="*" element={<h1 className="title">Page not found</h1>} />
+      </Route>
+    </Routes>
+  </HashRouter>
 );
+
+createRoot(document.getElementById('root') as HTMLElement).render(<Root />);
