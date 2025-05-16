524867e341beda009ab07e0cd98852fb10536136
Alex Marcovych <alexmarcovych@gmail.com>
2025-05-16 17:04:22 +0300

    new commit

--- src/index.tsx
+++ src/index.tsx
@@ -8,10 +8,7 @@ const Root = () => (
     <Routes>
       <Route path="/" element={<App />}>
         <Route index element={<h1 className="title">Home page</h1>} />
-        <Route path="tabs">
-          <Route index element={<TabsPage />} />
-          <Route path=":tabId?" element={<TabsPage />} />
-        </Route>
+        <Route path="tabs/:tabId?" element={<TabsPage />} />
         <Route path="/home" element={<Navigate to="/" replace />} />
         <Route path="*" element={<h1 className="title">Page not found</h1>} />
       </Route>
