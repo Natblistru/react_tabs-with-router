2fb79eb21f5f48e072e1f79bbd1eef194312446d
Tim <drdakkadakka@gmail.com>
2025-05-21 12:25:34 +0200

    add base markup

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,18 @@
 import { createRoot } from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import { App } from './App';
+import { Homepage } from './pages/Homepage';
+import { Tabs } from './pages/Tabs';
+import { NotFound } from './pages/404';
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <HashRouter>
-    <App />
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route index element={<Homepage />} />
+        <Route path="tabs" element={<Tabs />} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
   </HashRouter>,
 );
