eddf2b6a049ddfceddbd307dd0379ca5e4f5eef5
tsiupaknazar <sjprime280@gmail.com>
2025-06-10 17:07:14 +0100

    added task solution

---
+++ src/components/Nav.tsx
@@ -0,0 +1,35 @@
+import { FC } from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Nav: FC = () => {
+  return (
+    <nav
+      className="navbar is-light is-fixed-top is-mobile has-shadow"
+      data-cy="Nav"
+    >
+      <div className="container">
+        <div className="navbar-brand">
+          <NavLink
+            to="/"
+            end
+            className={({ isActive }) =>
+              `navbar-item ${isActive ? 'is-active' : ''}`
+            }
+          >
+            Home
+          </NavLink>
+          <NavLink
+            to="/tabs"
+            className={({ isActive }) =>
+              `navbar-item ${isActive ? 'is-active' : ''}`
+            }
+          >
+            Tabs
+          </NavLink>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react';
+
+const Home: FC = () => {
+  return (
+    <div>
+      <h1 className="title">Home page</h1>
+    </div>
+  );
+};
+
+export default Home;
