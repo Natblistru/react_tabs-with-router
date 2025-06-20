96fd4ed0c5023afb736e4ceee57dbc31d646e7d3
Andrii Chornyi <ixxxx@i.ua>
2025-06-20 17:12:57 +0300

    Task Solution

--- src/components/Tabs.tsx
+++ src/components/Tabs.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useState } from 'react';
 import { Tab } from '../types/Tab';
 import { Routes, Route, Link } from 'react-router-dom';
 
@@ -7,14 +8,16 @@ type Props = {
 };
 
 export const Tabs: React.FC<Props> = ({ tabs }) => {
+
   return (
     <>
       <div className="tabs is-boxed">
         <ul>
           {tabs.map(tab => {
+
             return (
               <li data-cy="Tab" className="is-active" key={tab.id}>
-                <Link to="#/">{tab.title}</Link>
+                <Link to={`#/${tab.id}`}>{tab.title}</Link>
               </li>
             );
           })}
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
 import { App } from './App';
-import { TabsContent } from ''
+// import { TabsContent } from ''
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <HashRouter>
