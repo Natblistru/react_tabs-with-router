4a720b8c8751768b7bfa1165214363c636dfc93c
Denis Pitsul <denia.pitsul@gmail.com>
2025-06-20 11:50:41 +0300

    add react tabs with router solution

---
+++ src/Root.tsx
@@ -0,0 +1,21 @@
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { App } from './App';
+import { HomePage } from './pages/HomePage';
+import { NotFoundPage } from './pages/NotFoundPage';
+import { TabsPage } from './pages/TabsPage';
+
+export const Root = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route index element={<HomePage />} />
+        <Route path="/home" element={<Navigate to="/" replace />} />
+        <Route path="/tabs">
+          <Route index element={<TabsPage />} />
+          <Route path=":tabId?" element={<TabsPage />} />
+        </Route>
+        <Route path="*" element={<NotFoundPage />} />
+      </Route>
+    </Routes>
+  );
+};
