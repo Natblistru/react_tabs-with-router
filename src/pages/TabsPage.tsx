f934be04702b7372cfbeb0e38a7ad002781402aa
unknown <vladhafarov@gmail.com>
2025-06-22 21:12:26 +0300

    fixed

--- src/pages/TabsPage.tsx
+++ src/pages/TabsPage.tsx
@@ -1,3 +1,43 @@
+// import classNames from 'classnames';
+// import { Link, useParams } from 'react-router-dom';
+
+// const tabs = [
+//   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
+//   { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
+//   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
+// ];
+
+// export const TabsPage = () => {
+//   const { tabId } = useParams();
+//   const selectedTab = tabs.find(tab => tab.id === tabId);
+
+//   return (
+//     <>
+//       <h1 className="title">Tabs page</h1>
+
+//       <div className="tabs is-boxed">
+//         <ul>
+//           {tabs.map(tab => {
+//             return (
+//               <li
+//                 data-cy="Tab"
+//                 className={classNames({ 'is-active': tabId === tab.id })}
+//                 key={tab.id}
+//               >
+//                 <Link to={`/tabs/${tab.id}`}>{tab.title}</Link>
+//               </li>
+//             );
+//           })}
+//         </ul>
+//       </div>
+
+//       <div className="block" data-cy="TabContent">
+//         {selectedTab ? selectedTab?.content : 'Please select a tab'}
+//       </div>
+//     </>
+//   );
+// };
+
 import classNames from 'classnames';
 import { Link, useParams } from 'react-router-dom';
 
@@ -8,7 +48,8 @@ const tabs = [
 ];
 
 export const TabsPage = () => {
-  const { tabId } = useParams();
+  const { tabId } = useParams<{ tabId?: string }>();
+
   const selectedTab = tabs.find(tab => tab.id === tabId);
 
   return (
@@ -32,7 +73,7 @@ export const TabsPage = () => {
       </div>
 
       <div className="block" data-cy="TabContent">
-        {selectedTab ? selectedTab?.content : 'Please select a tab'}
+        {selectedTab ? selectedTab.content : 'Please select a tab'}
       </div>
     </>
   );
