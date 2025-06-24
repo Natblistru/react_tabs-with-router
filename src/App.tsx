3c3297269b219fe985d8b6b7a1b660dd1a526a93
Andrii Chornyi <ixxxx@i.ua>
2025-06-24 11:27:28 +0300

    Task Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,35 @@
-import { Routes, Route, Link, useLocation } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import './App.scss';
+
 import { HomeTitle } from './components/HomePage';
 import { Tabs } from './components/Tabs';
-import cn from 'classnames';
+import { Navigation } from './components/Navigation';
 
 export const App = () => {
-  const validPage = ['/tabs/tab-1', '/tabs/tab-2', '/tabs/tab-3', '/tabs', '/'];
-  const location = useLocation();
-  const isValid = validPage.includes(location.pathname);
-
-  let activ = validPage.find(valid => valid === location.pathname) || useLocation;
-  if (activ) {
-    activ = activ.toString()
-  }
-
-  console.log('activ-----activ', activ);
-
   return (
     <>
-      {/* Also requires <html class="has-navbar-fixed-top"> */}
-
-      {<nav
-        className="navbar is-light is-fixed-top is-mobile has-shadow"
-        data-cy="Nav"
-      >
-        <div className="container">
-          <div className="navbar-brand">
-
-          <Link to="/" className={cn("navbar-item", {'is-active': activ ==='/'})}>
-            Home
-          </Link>
-          <Link to="/tabs" className={cn("navbar-item", {'is-active': activ.length > 1})}>
-            Tabs
-          </Link>
-
-        </div>
-      </div>
-      </nav>}
+      <Navigation />
 
       <div className="section">
         <div className="container">
           <Routes>
-            <Route path="/:tabs?" element={<HomeTitle />} />
+            <Route path="/home" element={<Navigate to="/" replace />} />
+
+            <Route path="/" element={<HomeTitle />} />
 
             <Route
               path="tabs/:tabsId?"
               element={
-                isValid ? (
-                  <>
-                    <HomeTitle />
-                    <Tabs />{' '}
-                  </>
-                ) : (
+                <>
                   <HomeTitle />
-                )
+                  <Tabs />
+                </>
               }
             />
+
+            <Route path="*" element={<h1 className="title">Page not found</h1>} />
           </Routes>
         </div>
       </div>
--- src/components/Tabs.tsx
+++ src/components/Tabs.tsx
@@ -1,34 +1,28 @@
-// import React, { useEffect } from 'react';
-// import { useState } from 'react';
-// import { Tab } from '../types/Tab';
 import { Link, useParams } from 'react-router-dom';
 import cn from 'classnames';
 
-type Props = {
-  // tabs: Tab[];
-
-};
-
-const tabs = [
+const tabsData = [
   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
   { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
 ];
 
-export const Tabs: React.FC<Props> = () => {
-  const { tabsId } = useParams(); // Получаем id вкладки из URL
-  const selectedTabId = tabsId || ''; // Строка. Например, 'tab-1'
+export const Tabs = () => {
+  const { tabsId } = useParams();
+  const selectedTab = tabsData.find(tab => tab.id === tabsId);
 
   return (
-
     <>
+
       <div className="tabs is-boxed">
         <ul>
-          {tabs.map(tab => (
+          {tabsData.map(tab => (
             <li
-              data-cy="Tab"
               key={tab.id}
-              className={cn({ 'is-active': tab.id === selectedTabId })} // Подсвечиваем активную вкладку
+              data-cy="Tab"
+              className={cn({
+                'is-active': tab.id === tabsId && selectedTab,
+              })}
             >
               <Link to={`/tabs/${tab.id}`}>{tab.title}</Link>
             </li>
@@ -37,9 +31,11 @@ export const Tabs: React.FC<Props> = () => {
       </div>
 
       <div className="block" data-cy="TabContent">
-        {selectedTabId
-          ? tabs.find(tab => tab.id === selectedTabId)?.content
-          : 'Please select a tab'}
+        {selectedTab ? (
+          <p>{selectedTab.content}</p>
+        ) : (
+          <p>Please select a tab</p>
+        )}
       </div>
     </>
   );
